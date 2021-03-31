9454ebf10d5a4748c01424a30522de50573e5dd1
Johannes <johannes.bluhm@outlook.com>
2021-03-31 12:53:07 +0200

    added ip option

--- Network Prototype/Assets/Scripts/HelloWorldManager.cs
+++ Network Prototype/Assets/Scripts/HelloWorldManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Transports.UNET;
 using UnityEngine;
 
 namespace HelloWorld {
     public class HelloWorldManager : MonoBehaviour {
+    private static string ip = "127.0.0.1";
         void OnGUI() {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
             if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
@@ -20,9 +22,14 @@ namespace HelloWorld {
         }
 
         static void StartButtons() {
+            ip = GUILayout.TextField(ip);
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+            if (GUILayout.Button("Client") && ip != "") {
+                Debug.Log(ip);
+                NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = ip;
+                NetworkManager.Singleton.StartClient();
+            }
+            //if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
         static void StatusLabels() {
