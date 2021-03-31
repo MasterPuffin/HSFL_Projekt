4fe8bdaf4aea1e06518f7c7a34e7d389f8b751ba
Johannes <johannes.bluhm@outlook.com>
2021-03-31 16:01:47 +0200

    added camera

--- Network Prototype/Assets/Scripts/HelloWorldPlayer.cs
+++ Network Prototype/Assets/Scripts/HelloWorldPlayer.cs
@@ -18,7 +18,19 @@ namespace HelloWorld {
         private float gravityValue = -9.81f;
         private bool jumping = false;
 
+        public float mouseSensitivity = 50.0f;
+        public float clampAngle = 80.0f;
+
+        private float rotY = 0.0f; // rotation around the up/y axis
+        private float rotX = 0.0f; // rotation around the right/x axis
+
+        private Vector2 mouse;
+
         void Start() {
+            Camera.main.enabled = false;
+            Vector3 rot = transform.localRotation.eulerAngles;
+            rotY = rot.y;
+            rotX = rot.x;
             controller = GetComponent<CharacterController>();
         }
 
@@ -32,10 +44,26 @@ namespace HelloWorld {
             jumping = true;
         }
 
+        public void OnCamera(InputValue input) {
+            Debug.Log(input.Get<Vector2>());
+            mouse = input.Get<Vector2>();
+        }
+
         void Update() {
             if (IsLocalPlayer) {
+                Look();
                 MovePlayer();
             }
+        }
+
+        private void Look() {
+            rotY += mouse.x * mouseSensitivity * Time.deltaTime;
+            rotX += mouse.y * mouseSensitivity * Time.deltaTime;
+
+            rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+            Quaternion localRotation = Quaternion.Euler(rotX, rotY * -1, 0.0f);
+            transform.rotation = localRotation;
         }
 
         private void MovePlayer() {
