52ed6635e5d325c5eb16c1054471186bd231ab7d
JannikJ1 <jannik.janke@web.de>
2021-09-17 01:14:31 +0200

    Added monster to canyons + Removed some doors

--- main-build/Phase Alpha/Assets/Scripts/AgentController.cs
+++ main-build/Phase Alpha/Assets/Scripts/AgentController.cs
@@ -1,28 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class AgentController : MonoBehaviour
 {
     public NavMeshAgent agent;
     private float distanceP1;
+    private float distanceP2;
     private float distanceRandomPoint;
-    private GameObject player1;
-    private GameObject player2;
+    private GameObject[] players;
+    //private GameObject player2;
     public GameObject[] randomPoints;
     public GameObject currentPoint;
+    private bool cr_running;
+    private GameObject closestPlayer;
 
     private int index;
 
     private bool hasAggro;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (randomPoints.Length<=0)
+
+        if (randomPoints.Length <= 0)
         {
-            
-            randomPoints = GameObject.FindGameObjectsWithTag("MonsterWalkPoint");
+            if (this.gameObject.name == "Monster")
+            {
+                randomPoints = GameObject.FindGameObjectsWithTag("MonsterWalkPoint");
+            }
+            else
+            {
+                randomPoints = GameObject.FindGameObjectsWithTag("MonsterWalkPointCanyons");
+            }
             index = Random.Range(0, randomPoints.Length);
             currentPoint = randomPoints[index];
             agent.SetDestination(currentPoint.transform.position);
@@ -33,16 +47,37 @@ public class AgentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player1 ==  null)
+        distanceRandomPoint = Vector3.Distance(currentPoint.transform.position, transform.position);
+        //Debug.Log(distanceRandomPoint);
+        if (players == null)
         {
-            player1 = GameObject.Find("Player(Clone)");
+            players = GameObject.FindGameObjectsWithTag("Player");
+        }
+        else if (players.Length != 2)
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+
         }
         else
         {
-            distanceP1 = Vector3.Distance(player1.transform.position, transform.position);
-            distanceRandomPoint = Vector3.Distance(currentPoint.transform.position, transform.position);
+            distanceP1 = Vector3.Distance(players[0].transform.position, transform.position);
+            distanceP2 = Vector3.Distance(players[1].transform.position, transform.position);
+            
+            
+            if (distanceP1 < distanceP2)
+            {
+                closestPlayer = players[0];
+            }
+            else
+            {
+                closestPlayer = players[1];
+            }
 
-            if (distanceP1 < 5)
+            if (Vector3.Distance(closestPlayer.transform.position, transform.position) < 5)
+            {
+                closestPlayer.GetComponent<PlayerController>().KillPlayer();
+            }
+            else if (Vector3.Distance(closestPlayer.transform.position, transform.position) < 15)
             {
                 hasAggro = true;
             }
@@ -50,22 +85,25 @@ public class AgentController : MonoBehaviour
             {
                 hasAggro = false;
             }
-
-            if (hasAggro)
-            {
-                //agent.isStopped = false;
-                agent.SetDestination(player1.transform.position);
-            }
-            else if(distanceRandomPoint <= 3)
-            {
-                index = Random.Range(0, randomPoints.Length);
-                //Debug.Log(index);
-                currentPoint = randomPoints[index];
-            }else
-            {
-                agent.SetDestination(currentPoint.transform.position);
-            }
         }
-        //Debug.Log(player1);
+        if (hasAggro)
+        {
+            //agent.isStopped = false;
+            agent.SetDestination(closestPlayer.transform.position);
+        }
+        else if (distanceRandomPoint <= 5.0f)
+        {
+            //Debug.Log("close to point");
+            index = Random.Range(0, randomPoints.Length);
+            currentPoint = randomPoints[index];
+            
+        }
+        else
+        {
+            //Debug.Log("set new target point");
+            agent.SetDestination(currentPoint.transform.position);
+        }
+
     }
+
 }
